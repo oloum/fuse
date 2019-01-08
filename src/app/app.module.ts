52c1013b930783ff92284af11aa2b6a096a1983e
ndiogousamb <sambndiogou05@gmail.com>
2019-01-08 13:25:32 +0000

    modif routage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 import 'hammerjs';
-import {MatTableModule} from '@angular/material/table';
-import {MatSortModule} from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
 import { FuseModule } from '@fuse/fuse.module';
 import { FuseSharedModule } from '@fuse/shared.module';
 import { fuseConfig } from './fuse-config';
@@ -15,9 +15,11 @@ import { AppComponent } from './app.component';
 import { FuseMainModule } from './main/main.module';
 import { FuseSampleModule } from './main/content/sample/sample.module';
 import { MatButtonModule, MatFormFieldModule, MatInputModule, MatRippleModule } from '@angular/material';
+import { FuseContentComponent } from './main/content/content.component';
 const appRoutes: Routes = [
+    { path: 'sample1', component: FuseContentComponent },
     {
-        path      : '**',
+        path: '**',
         redirectTo: 'sample'
     }
 ];
@@ -25,12 +27,12 @@ const appRoutes: Routes = [
 @NgModule({
     declarations: [
         AppComponent,
-        
+
     ],
     schemas: [
         CUSTOM_ELEMENTS_SCHEMA
     ],
-    imports     : [
+    imports: [
         BrowserModule,
         BrowserAnimationsModule,
         HttpClientModule,
@@ -44,7 +46,7 @@ const appRoutes: Routes = [
         MatRippleModule,
 
         // Fuse Main and Shared modules
-        
+
         FuseModule.forRoot(fuseConfig),
         FuseSharedModule,
         FuseMainModule,
@@ -63,10 +65,9 @@ const appRoutes: Routes = [
         FuseMainModule,
         FuseSampleModule
     ],
-    bootstrap   : [
+    bootstrap: [
         AppComponent
     ]
 })
-export class AppModule
-{
+export class AppModule {
 }
